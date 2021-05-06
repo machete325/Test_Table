d620c6be88f8b010d0ccc9e195dd0c738fa8d6f9
machete325 <machete325@gmail.com>
2021-05-06 18:32:06 +0300

    Додано React DevTools

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -1,10 +1,12 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 import tableReducer from './table_reducer';
 
 let reducers = combineReducers({
   table: tableReducer,
 });
 
-let store = createStore(reducers);
+let store = createStore(reducers, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
